f6ac529f51e551537a1292d23a49d4f3dc43cfff
kyungji12 <kyungji12@gmail.com>
2021-01-26 15:35:11 +0900

    renamed main.html to home.html

--- ACYN_dev/acyn_project/acyn_project/urls.py
+++ ACYN_dev/acyn_project/acyn_project/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webCrawlApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', view.home, name="home"),
+    path('', views.home, name="home"),
 ]
--- ACYN_dev/acyn_project/webCrawlApp/views.py
+++ ACYN_dev/acyn_project/webCrawlApp/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render #html파일에 원하는 context인자를 보낼 수 있음
+from django.shortcuts import redirect #url만 이동하는 것
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
