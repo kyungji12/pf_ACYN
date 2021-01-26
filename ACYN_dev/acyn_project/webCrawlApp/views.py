a41df5d3bd04818bafd4851f3519d82085001a87
kyungji12 <kyungji12@gmail.com>
2021-01-26 16:47:29 +0900

    done with form action

--- ACYN_dev/acyn_project/webCrawlApp/views.py
+++ ACYN_dev/acyn_project/webCrawlApp/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render #html파일에 원하는 context인자를 보낼 수 있음
 from django.shortcuts import redirect #url만 이동하는 것
 
+from django.http import HttpResponse
+
 # Create your views here.
 
 def home(request):
     return render(request, 'main.html')
 
 def result(request):
-
     input_val = request.POST['input_val']
-    print("🥲", input_val)
-
-    return redirect(request, 'result.html')
+    # print("🥲", input_val)
+    context = {
+        'input_val' : input_val
+    }
+    return render(request, 'result.html', context)
