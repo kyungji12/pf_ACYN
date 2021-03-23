133ed744bdc5fa217e5673bbc0223f8e1e224c6e
kyungji12 <kyungji12@gmail.com>
2021-03-23 10:21:43 +0900

    movie app in React.js almost done

--- react_first/movie_app/src/App.js
+++ react_first/movie_app/src/App.js
@@ -2,8 +2,22 @@ import React from 'react';
 import PropTypes from "prop-types";
 
 class App extends React.Component {
+
+  state = {
+    isLoading : true,
+    movies: []
+  };
+  componentDidMount() {
+    setTimeout(()=>{
+      this.setState({isLoading: false})
+    }, 6000);
+  }
+
   render() {
-    return <h1>I am a class componet</h1>
+    const {isLoading} = this.state;
+    return <div>
+      {isLoading ? "Loading..." : "We are ready"}
+    </div>
   }
 }
 
